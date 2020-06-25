c0c062d4e9398541d197893ac073626178fe9f7e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-25 16:58:23 +0530

    app.java

--- ethereum/src/main/java/web3j/ethereum/Application.java
+++ ethereum/src/main/java/web3j/ethereum/Application.java
@@ -1,45 +1,64 @@
 package web3j.ethereum;
 
 
-import org.blockchain.config.AppContext;
-import org.web3j.crypto.WalletFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jService;
-import org.web3j.protocol.core.DefaultBlockParameterNumber;
-import org.web3j.protocol.core.Request;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.protocol.parity.Parity;
-import org.web3j.protocol.parity.methods.response.ParityExportAccount;
-import org.web3j.protocol.parity.methods.response.ParityTracesResponse;
-import org.web3j.protocol.parity.methods.response.Trace;
+import org.web3j.tx.Contract;
+import org.web3j.tx.ManagedTransaction;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
+
+import java.math.BigDecimal;
 
 import java.util.List;
 
 public class Application {
-    private static Web3jService service = new HttpService("http://localhost:8545"); // put fullnode url here
-    private static Web3j web3j = Web3j.build(service);
-    private static Parity parity = Parity.build(service);
+	  private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static List<Trace> getCallActionsInBlock(long blockNum) throws Exception {
-        DefaultBlockParameterNumber number = new DefaultBlockParameterNumber(blockNum);
-        Request<?, ParityTracesResponse> request = parity.traceBlock(number);
-        ParityTracesResponse response = request.send();
-        return response.getTraces();
-    }
+	    public static void main(String[] args) throws Exception {
+	        new Application().run();
+	    }
 
-    public static List<Trace> getCallAction(String hash) throws Exception {
-        Request<?, ParityTracesResponse> request = parity.traceTransaction(hash);
-        ParityTracesResponse response = request.send();
-        return response.getTraces();
-    }
+	    private void run() throws Exception {
+	        // We start by creating a new web3j instance to connect to remote nodes on the network.
+	        Web3j web3j = Web3j.build(new HttpService());
+	        log.info("Connected to Ethereum client version: "
+	                + web3j.web3ClientVersion().send().getWeb3ClientVersion());
+	        Credentials credentials =
+	                WalletUtils.loadCredentials(
+	                        "password",
+	                        "chaindata\\keystore\\UTC--2018-06-21T06-34-32.658490800Z--5ade9a7f8f57ab3995ac4d56c78a22649d3b1686");
+	        log.info("Credentials loaded");
+	        log.info("Sending Ether ..");
+	        TransactionReceipt transferReceipt = Transfer.sendFunds(
+	                web3j, credentials,
+	                "0xDc1Fb60F7E1eF6ef052A014EFbDF82debd803D06",  // you can put any address here
+	                BigDecimal.valueOf(100), Convert.Unit.ETHER)  // 1 wei = 10^-18 Ether
+	                .sendAsync().get();
+	        log.info("Transaction complete : "
+	                + transferReceipt.getTransactionHash());
 
-    public static WalletFile exportAccount(String address, String password) throws Exception {
-        Request<?, ParityExportAccount> request = parity.parityExportAccount(address, password);
-        ParityExportAccount response = request.send();
-        return response.getWallet();
-    }
 
-    public static void main(String[] args) throws Exception {
-// test function here
-    }
-}
+	        // Now lets deploy a smart contract
+	        log.info("Deploying smart contract");
+	        Helloworld contract = Helloworld.deploy(
+	                web3j, credentials,
+	                ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT).send();
+
+	        String contractAddress = contract.getContractAddress();
+	        log.info("Smart contract deployed to address " + contractAddress);
+
+	        log.info("Initial value of counter in Smart contract: " + contract.getCounter().send());
+	        log.info("Incrementing counter in Smart contract");
+	        contract.add().send();
+	        log.info("Value of counter in Smart contract after increment : " + contract.getCounter().send());
+	        log.info("Decrementing counter in Smart contract");
+	        contract.subtract().send();
+	        log.info("Final value of counter in Smart contract : " + contract.getCounter().send());
+	    }
+	}
